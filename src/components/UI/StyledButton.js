a5ff7c336ad20dd78b313d6f65df3cc649c4ec94
chokolove <carlosll2493@gmail.com>
2021-03-25 19:14:53 -0500

    Add StyledButton

---
+++ src/components/UI/StyledButton.js
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export default styled.button`
+  padding: 8px 16px;
+  background: #2F80ED;
+  border-radius: 8px;
+  color: white;
+  border: 1px transparent;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 24px;
+  &:focus {
+    outline: none;
+  }
+`;
