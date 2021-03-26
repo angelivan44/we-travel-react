ff8f0affebaf20bfce2ee38c79c66ac239d197a6
chokolove <carlosll2493@gmail.com>
2021-03-25 19:15:19 -0500

    Add basic Navbar

---
+++ src/components/UI/Navbar.js
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { Link, useLocation } from "react-router-dom";
+import Button from "./StyledButton";
+import InputField from "./FormField";
+
+export default function Navbar() {
+  const location = useLocation();
+
+  return (
+    <div css={css`
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      & h1{
+        margin: 0;
+      }
+    `}>
+      <div><h1>Brand</h1></div>
+      <div>
+        <InputField>
+          <input placeholder="Search" />
+        </InputField>
+      </div>
+      <div>
+        <Button>Log In</Button>
+        <Button>Create Account</Button>
+      </div>
+    </div>
+  );
+}
