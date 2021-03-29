35c1032684ec558b9b0075244b8584a7996d399a
chokolove <carlosll2493@gmail.com>
2021-03-29 16:34:52 -0500

    Remove extra navbar

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -5,7 +5,6 @@ import Navbar from "../components/UI/Navbar";
 export default function Category () {
   return (
     <div>
-      <Navbar />
       <StyledDiv>
         <SideBar />
       </StyledDiv>
