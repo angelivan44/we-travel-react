c9549477f60ebd6e78108cc6cf230476454efc34
Robert <robert.tm2018@gmail.com>
2021-03-29 13:57:46 -0500

    Added style for wrapper card Top blogger

--- src/components/Landing/TopBloggers.js
+++ src/components/Landing/TopBloggers.js
@@ -1,79 +1,76 @@
-import styled from "@emotion/styled"
-import BloggerCard from "../containers/BloggerCard"
-export default function TopBloggers () {
+import styled from "@emotion/styled";
+import BloggerCard from "../containers/BloggerCard";
+export default function TopBloggers() {
   const data = [
     {
       id: 1,
-      img: "https://via.placeholder.com/150",
-      name: "Robert TM",
-      github: "google.com",
-      linkedin: "google.com"
+      avatar:
+        "https://res.cloudinary.com/robcar/image/upload/v1613786735/image_1_cyn7va.png",
+      name: "@Chokolove",
+      description:
+        "Suspendisse Ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat toyror nutlrices massa turpis. Arcu pulvinar Benean nam laoreet nulla",
     },
     {
       id: 2,
-      img: "https://via.placeholder.com/150",
-      name: "Andromeda_019",
-      github: "google.com",
-      linkedin: "google.com"
+      avatar: "",
+      name: "@Andromeda_019",
+      description:
+        "Suspendisse Ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat toyror nutlrices massa turpis. Arcu pulvinar Benean nam laoreet nulla, Suspendisse Ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat toyror nutlrices massa turpis. Arcu pulvinar Benean nam laoreet nulla",
     },
     {
       id: 3,
-      img: "https://via.placeholder.com/150",
-      name: "Angel Huayas",
-      github: "google.com",
-      linkedin: "google.com"
+      avatar:
+        "https://res.cloudinary.com/robcar/image/upload/v1613786735/image_1_cyn7va.png",
+      name: "@Angel Huayas",
+      description:
+        "Suspendisse Ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat toyror nutlrices massa turpis. Arcu pulvinar Benean nam laoreet nulla",
     },
-    {
-      id: 4,
-      img: "https://via.placeholder.com/150",
-      name: "Chokolove",
-      github: "google.com",
-      linkedin: "google.com"
-    },
-  ]
-  
+  ];
+
   return (
-    <StyleDiv>
+    <StyleWrapper>
       <h1>Top Bloggers</h1>
-      <StyleWrapperCard>
-      <BloggerCard
-        src={"https://www.playas-peru.com/wp-content/uploads/2019/01/playas-en-piura-3.jpg"}
-        nameUser={"@John"}
-        content={
-          "Suspendisse Ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat toyror nutlrices massa turpis. Arcu pulvinar Benean nam laoreet nulla"
-        }
-      ></BloggerCard>
-      <BloggerCard
-        
-        nameUser={"@John"}
-        content={
-          "Suspendisse Ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat toyror nutlrices massa turpis. Arcu pulvinar Benean nam laoreet nulla, Suspendisse Ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat toyror nutlrices massa turpis. Arcu pulvinar Benean nam laoreet nulla"
-        }
-      ></BloggerCard>
-      </StyleWrapperCard>
-
-    </StyleDiv>
-  )
-
+      <StyleDiv>
+        <StyleWrapperCard>
+          {data.map((user) => (
+            <BloggerCard
+              src={user.avatar}
+              nameUser={user.name}
+              content={user.description}
+            ></BloggerCard>
+          ))}
+        </StyleWrapperCard>
+      </StyleDiv>
+    </StyleWrapper>
+  );
 }
 
-const StyleDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items:center;
-  justify-content: center;
-  gap:20px;
-  padding: 20px 65px 65px 65px;
-  background: #A1683A;
-  & > h1{
-    color: #FFFFFF;
+const StyleWrapper = styled.div`
+  background: #a1683a;
+  padding: 20px 20px 65px 20px;
+  & > h1 {
+    color: #ffffff;
+    text-align: center;
     font-weight: bold;
     font-size: 34px;
     line-height: 41px;
   }
-`
+`;
 
-const StyleWrapperCard= styled.div`
+const StyleDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  padding: 20px;
+  overflow: auto;
+  
+`;
+
+const StyleWrapperCard = styled.div`
   display: flex;
   gap: 16px;
-`
+  margin: auto;
+  padding-right: 60px;
+`;
